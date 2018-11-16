50e8016c9245066060aa7da62dd737d5f5142c67
danialbleile <danial.bleile@wsu.edu>
2018-11-16 13:56:11 -0800

    Move Breadcrumbs

---
+++ parts/single-layout.php
@@ -0,0 +1,24 @@
+<?php if ( ! is_front_page() && ! is_home() && spine_display_breadcrumbs( 'bottom' ) ) {
+	?><section class="row single breadcrumbs breadcrumbs-bottom gutter pad-top" typeof="BreadcrumbList" vocab="http://schema.org/">
+	<div class="column one"><?php bcn_display(); ?></div>
+	</section><?php
+} ?>
+<section class="row side-right gutter pad-ends">
+
+	<div class="column one">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'articles/post', get_post_type() ) ?>
+
+		<?php endwhile; ?>
+
+	</div><!--/column-->
+
+	<div class="column two">
+
+		<?php get_sidebar(); ?>
+
+	</div><!--/column two-->
+
+</section>
