76e2ac4ba067db142c6384c1d9bb6f2bb21e8ecf
danialbleile <danial.bleile@wsu.edu>
2018-12-05 15:02:16 -0800

    Fix footer

--- parts/after-binder.php
+++ parts/after-binder.php
@@ -1,4 +1,4 @@
-<footer role="footer" class="main-foot equalize">
+<footer class="main-foot equalize">
 <?php if ( is_active_sidebar( 'site_footer' ) ) : ?>
 	<?php dynamic_sidebar( 'site_footer' ); ?>
 <?php endif ?>
